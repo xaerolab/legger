1f2b4f4e302c3d4d5feb5b51ef96a6f7480214b2
sergio <zelgady@gmail.com>
2022-08-10 19:40:58 -0500

    se empieza a realizar la validacion del formulario con javascript

--- front/assets/js/functions.js
+++ front/assets/js/functions.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
     console.log('Bienvenidos a esta landing!! 👽');
 });
+
+//validacion de formulario
+$('#register').on('click',function(e){
+    e.preventDefault();
+
+    nombre = $('#nombre').val();
+
+    regexNombre = /^[a-zA-Záéíóúñ ]+$/;
+
+	if(!nombre){
+		errNombre = '* Campo requerido';
+		$('#errNombre').html(errNombre);
+	}else if(!regexNombre.test(nombre)){
+		errNombre = '* Caracter no válido';
+		$('#errNombre').html(errNombre);
+	}else{
+		errNombre = '';
+		$('#errNombre').html(errNombre);
+		nombre = $('#nombre').val();
+	}
+    
+
+});
